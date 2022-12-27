ff92e913379baf24761d13e3ac4c084a7aac89ed
mehmettirpan <mehmettirpan92@gmail.com>
2022-12-27 17:45:07 +0300

    main

---
+++ StrCpyCode/StrCpyCode/strcpy.c
@@ -0,0 +1,39 @@
+//
+//  main.c
+//  delete
+//
+//  Created by Mehmet TIRPAN on 27.12.2022.
+//
+
+#include <stdio.h>
+
+void strcopy(char *first,char *second)
+{
+    int i = 0;
+    while (*(first+i)!='\0'){
+        *(first+i) = *(second+i);
+        i++;
+    }
+}
+
+int main()
+{
+    char str1[20];
+    char str2[20];
+    char str3[20] = "                    ";
+    
+    printf("Enter First String: ");
+        scanf("%s",&str1);
+    printf("Enter Second String: ");
+        scanf("%s",&str2);
+    printf("\nEntered String: ");
+    printf("%s  %s\n\n",str1,str2);
+
+    strcopy(str3,str1);
+    strcopy(str1,str2);
+    strcopy(str2,str3);
+    printf("Changed String: ");
+    printf("%s %s \n\n",str1, str2);
+    
+    return 0;
+}
